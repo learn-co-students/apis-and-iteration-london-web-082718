5f51c5f8b85ffb8347cad7b9be453f537cee1ebf
thexyman <makdaa1@gmail.com>
2018-08-29 14:48:23 +0100

    finsihed building kickass star wars api

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,21 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to our amazing star wars program!"
 end
 
-def get_character_from_user
-  puts "please enter a character"
+def display_menu
+  puts "Please choose from the following options to use the app:"
+  puts "'1' to search for character"
+  puts "'2' to search for movie info"
+  puts "'q' to quit app"
+  gets.chomp.downcase
+end
+
+def get_user_choice(entry)
+  puts "Please enter #{entry}:"
   # use gets to capture the user's input. This method should return that input, downcased.
+  capitalizer(gets.chomp.downcase)
+end
+
+def capitalizer(string)
+  string.split.map(&:capitalize).join(" ")
 end
