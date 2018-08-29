8087eaee3aeba48d82250def53a45cf2b2582a16
Billie Simmons <billiesnyc@gmail.com>
2018-08-29 15:59:44 +0000

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,8 +6,8 @@ def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
 
 
@@ -20,18 +20,35 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
+  # p response_hash["results"]
+  response_hash["results"].each do |item|
+      if character == item["name"].downcase
+        item["films"].each do |link|
+          film_string = RestClient.get(link)
+          film_hash = JSON.parse(film_string)
+          print_movies(film_hash)
+        end
+      end
+  end
 end
+
 
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+
+  p "#{films_hash["title"]}"
+  # films_hash.each do |film|
+  #   film.each do |key, value|
+  #     if key == "title"
+  #       puts value
+  #     end
+  #   end
+  # end
 end
+
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
-  print_movies(films_array)
+  #print_movies(films_array)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
